9a6f0ebfb93ddf8ec37f809c4eabba92293ac008
Felipe Vasconcelos Assunção <felipe.assuncao@eldorado.org.br>
2025-09-10 02:28:50 -0400

    refactor: remove preview screens and HeaderNormalizer

--- app/src/main/java/home/felipe/water/pocket/analysis/ui/screens/preview/PreviewUiState.kt
+++
@@ -1,9 +0,0 @@
-package home.felipe.water.pocket.analysis.ui.screens.preview
-
-import home.felipe.domain.vo.PreviewSummary
-
-sealed class PreviewUiState {
-    data object Loading : PreviewUiState()
-    data class Error(val msg: String) : PreviewUiState()
-    data class Ready(val summary: PreviewSummary) : PreviewUiState()
-}
